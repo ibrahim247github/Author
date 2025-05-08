5a66c1c3a58bc6847a852ddb1981182037a3aacb
Ibrahim247github <ibrahimsaheed333@gmail.com>
2025-05-08 02:03:16 +0100

    Done with the about section

---
+++ js/carousel.js
@@ -0,0 +1,4 @@
+const carousel = document.querySelector(".carousel");
+const carouselSlider = document.querySelector(".carousel__slider");
+const nextBtn = document.querySelector(".carousel__next-btn");
+const prevBtn = document.querySelector(".carousel__prev-btn");
